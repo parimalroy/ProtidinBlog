97239b6092d4c64e64f2863b9feba4b444e93dbd
parimal kumar roy <parimal.tbd@gmail.com>
2017-08-19 19:16:21 +0600

    protidin 1

--- resources/views/admin/home/login.blade.php
+++ resources/views/admin/home/login.blade.php
@@ -13,18 +13,18 @@ License URL: http://creativecommons.org/licenses/by/3.0/
           Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
     <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
     <!-- bootstrap-css -->
-    <link rel="stylesheet" href="{{url(asset('public/admin/login'))}}/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="{{url(asset('public/admin/'))}}/css/bootstrap.min.css" >
     <!-- //bootstrap-css -->
     <!-- Custom CSS -->
-    <link href="{{url(asset('public/admin/login'))}}/css/style.css" rel='stylesheet' type='text/css' />
-    <link href="{{url(asset('public/admin/login'))}}/css/style-responsive.css" rel="stylesheet"/>
+    <link href="{{url(asset('public/admin/'))}}/css/style.css" rel='stylesheet' type='text/css' />
+    <link href="{{url(asset('public/admin/'))}}/css/style-responsive.css" rel="stylesheet"/>
     <!-- font CSS -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
     <!-- font-awesome icons -->
-    <link rel="stylesheet" href="{{url(asset('public/frontEnd/'))}}/css/font.css" type="text/css"/>
-    <link href="{{url(asset('public/admin/login'))}}/css/font-awesome.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="{{url(asset('public/admin/'))}}/css/font.css" type="text/css"/>
+    <link href="{{url(asset('public/admin/'))}}/css/font-awesome.css" rel="stylesheet"> 
     <!-- //font-awesome icons -->
-    <script src="{{url(asset('public/admin/login'))}}/js/jquery2.0.3.min.js"></script>
+    <script src="{{url(asset('public/admin/'))}}/js/jquery2.0.3.min.js"></script>
 </head>
 <body>
     <div class="log-w3">
@@ -42,12 +42,12 @@ License URL: http://creativecommons.org/licenses/by/3.0/
             <!--<p>Don't Have an Account ?<a href="registration.html">Create an account</a></p>-->
         </div>
     </div>
-    <script src="{{url(asset('public/admin/login'))}}/js/bootstrap.js"></script>
-    <script src="{{url(asset('public/admin/login'))}}/js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="{{url(asset('public/admin/login'))}}/js/scripts.js"></script>
-    <script src="{{url(asset('public/admin/login'))}}/js/jquery.slimscroll.js"></script>
-    <script src="{{url(asset('public/admin/login'))}}/js/jquery.nicescroll.js"></script>
+    <script src="{{url(asset('public/admin/'))}}/js/bootstrap.js"></script>
+    <script src="{{url(asset('public/admin/'))}}/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="{{url(asset('public/admin/'))}}/js/scripts.js"></script>
+    <script src="{{url(asset('public/admin/'))}}/js/jquery.slimscroll.js"></script>
+    <script src="{{url(asset('public/admin/'))}}/js/jquery.nicescroll.js"></script>
     <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
-    <script src="{{url(asset('public/admin/login'))}}/js/jquery.scrollTo.js"></script>
+    <script src="{{url(asset('public/admin/'))}}/js/jquery.scrollTo.js"></script>
 </body>
 </html>
